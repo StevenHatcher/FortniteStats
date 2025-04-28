56f7e6beab274bddb59d444e2e80469c7f4f97ac
StevenHatcher <stevenhatchersk@gmail.com>
2025-04-28 13:19:12 -0600

    updated get_player_stats
    Included defaults for the parameters and generalized the function to enable greater functionality

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import re
 import json # Used to get the json data from the tracker website and extract info
 import time
 
-# good for solo, duo, trio, squads, but not "all"
-stat_metadata_keys = ["TRNRating", "Score", "Top1", "Top3", "Top5", "Top6", "Top10", "Top12", "Top25", "KD", "WinRatio", "Matches", "Kills", "MinutesPlayed", "KPM", "KPG", "AvgTimePlayed", "ScorePerMatch", "ScorePerMin"]
-stat_metadata_keys_all = ["Score", "Top1", "KD", "WinRatio", "Matches", "Kills", "MinutesPlayed", "KPM", "KPG", "AvgTimePlayed", "ScorePerMatch", "ScorePerMin", "Top3_5_10", "TRNRating"]
+
 
 
 
@@ -41,7 +39,8 @@ def get_player_data(username):
 
         # get_player_stat(profile_data, "all", "all", "KD", "Value")
         # get_gamemode_stats(profile_data, platform=None, gamemode="all")
-        get_specific_stat_value(profile_data, platform=None, gamemode="all", stat="KD", option="value")
+        chosen_stats = get_player_stats(profile_data, category="stats", platform="preferred", gamemode="all", stat="lol", option=None)
+        print(chosen_stats)
      
     except Exception as e: # throw an error if there's a problem with the webpage
         print(f"Error: {e}")
@@ -50,127 +49,69 @@ def get_player_data(username):
     finally:
         driver.quit()
 
-# Function to return the value of a specific stat given the platform, gamemode, stat, and ********option of that stat
-def get_player_stat(profile_data, platform, gamemode, stat, option):
-
-    platform_section = None
-    if platform == "all":
-        platform = None
-    if gamemode == "all":
-        gamemode = None
-        
-    # find the section of the profile data that has the chosen platform as the correct platform
-    for section in profile_data["stats"]:
-        if section.get("platform") == platform or platform is None:
-            platform_section = section
-    # if theres no section with the chosen platform, return
-    if not platform_section:
-            print("Invalid platform input")
-            return
-            
-    if gamemode in platform_section["stats"]:
-        player_stats = platform_section["stats"][gamemode]
-    else:
-        print(f"{gamemode} not found!")
-        player_stats = []
-        
-    for stat in player_stats:
-        key = stat["metadata"]["key"]
-        if key == stat:
-            print("Stat_value: {stat[option]}")
-            return stat[option]
-             
-
-    # print(f"returned platform_section: {platform_section}")
+# Function to get data about the user, such as ID, etc.
+#category: platformInfo, userInfo, metadata
+def get_player_info(profile_data, category=None,):
     return
 
-def get_specific_stat_value(profile_data, platform, gamemode, stat, option):
-    """
-    Search the profile JSON for a given platform ("kbm", "gamepad", etc.) and gamemode ("solo", "duos", etc.).
-    If platform_type is None, it searches where "platform": null.
-    """
-    stats_list = profile_data.get("stats", [])
+
+# Function to return the value of a specific stat given the platform, gamemode, stat, and option of that stat
+def get_player_stats(profile_data, category="stats", ranked=False,platform=None, season=None, gamemode=None, stat=None, option=None):
+    
+   # If you want to get only the stats for the user's preferred platform, use preferred for the platform parameter in the funtcion call
+    if platform == "preferred":
+        platform = profile_data["metadata"]["preferredInputId"]
+    
+    stats_list = profile_data.get(category, [])  # get the stats section of the player profile
 
     # Find the correct platform section
     target_section = None
-    for section in stats_list:
-        if section.get("platform") == platform:
-            target_section = section
+    for section in stats_list: # the sections in the stats lists are divided by platform, so we iterate and check each one for the value in the platform parameter
+        if section.get("platform") == platform and section.get("season") == season and section.get("isCompetitive") == ranked:
+            target_section = section # if the current section is the section of the platform we're looking for, we're done looking
             break
 
-    if not target_section:
+    if not target_section: # If there aren't any sections for the fiven platform, print a message saying so and retunr
         print(f"No stats found for platform = {platform}")
         return None
 
-    # Now find the correct gamemode stats
-    stats_block = target_section.get("stats")
-    if not stats_block:
+
+
+    stats_block = target_section.get("stats") # get the stats section of the platform section we got previously
+    if not stats_block: # If that given platform doesn't have a stat block, return
         print(f"No stats block found for platform = {platform}")
         return None
+    if gamemode is None: return stats_block # If no gamemode is given, return the full stats block for the given platform
 
-    mode_stats = stats_block.get(gamemode)
+
+
+    mode_stats = stats_block.get(gamemode) # If a gamemode was given in function call, get the stats for that gamemode
     if not mode_stats:
         print(f"No gamemode '{gamemode}' found for platform = {platform}")
         return None
 
-    
 
-    for stats in mode_stats:
+
+    if stat is None: return mode_stats # If no stat is given, return the full gamemode stats
+    for stats in mode_stats:            #otherwise, loop through the stat sections until we either find the intended stat or not
         if stats["metadata"]["key"] == stat:
-            stat_value = stats[option]
-            break  # Stop looping after finding it
+            if option is not None:      # If the user didnt give an option for the stat (e.g. value, percentile, etc.), return all of that stat's data
+                return stats[option]
+            else: return stats
 
-    if stat_value is not None:
-        print(f"Stat Found: {stat_value}")
-    else:
-        print("stat not found in mode stats.")
-    
-    
-    return stat_value  # This will be a list of stats!
-
-
-def get_gamemode_stats(profile_data, platform, gamemode):
-    """
-    Search the profile JSON for a given platform ("kbm", "gamepad", etc.) and gamemode ("solo", "duos", etc.).
-    If platform_type is None, it searches where "platform": null.
-    """
-    stats_list = profile_data.get("stats", [])
-
-    # Find the correct platform section
-    target_section = None
-    for section in stats_list:
-        if section.get("platform") == platform:
-            target_section = section
-            break
-
-    if not target_section:
-        print(f"No stats found for platform = {platform}")
-        return None
-
-    # Now find the correct gamemode stats
-    stats_block = target_section.get("stats")
-    if not stats_block:
-        print(f"No stats block found for platform = {platform}")
-        return None
-
-    mode_stats = stats_block.get(gamemode)
-    if not mode_stats:
-        print(f"No gamemode '{gamemode}' found for platform = {platform}")
-        return None
-
-    print(f"Mode stats: {mode_stats}")
-    return mode_stats  # This will be a list of stats!
-
-
-
-def get_platform_stats():
     return
 
-def get_gamemode_stats():
-    return
-# platform: None for all, "touch", "kbm", "gamepad"
-# gamemode: "all", "duos", "squads", "trios", "solo", "ltm"
-# stat options up there in meta keys
+
+# Category: stats (default), last7DaysStats, last30DaysStats
+
+# platform: None "all", "touch", "kbm", "gamepad", "preferred"
+# gamemode: "all", "solo", "duos", "trios", "squads", "ltm"
+
+# Stats:
+# For "all" gamemode: 
+#   "TRNRating", "Score", "Top1", "Top3", "Top5", "Top6", "Top10", "Top12", "Top25", "KD", "WinRatio", "Matches", "Kills", "MinutesPlayed", "KPM", "KPG", "AvgTimePlayed", "ScorePerMatch", "ScorePerMin"
+# For "solo", "duos", "trios", "squads", "ltm" gamemodes:
+#   "Score", "Top1", "KD", "WinRatio", "Matches", "Kills", "MinutesPlayed", "KPM", "KPG", "AvgTimePlayed", "ScorePerMatch", "ScorePerMin", "Top3_5_10", "TRNRating"
 
 # OPTIONS FOR STATS BASED ON GAMEMODE AND STAT.
 
